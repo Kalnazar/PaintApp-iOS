7907bd4e108ef7b4a1a901f078b4011e6bf7321a
Sayat <102688997+Kalnazar@users.noreply.github.com>
2022-04-05 13:58:03 +0600

    Add files via upload

---
+++ PaintAppSayat/PaintAppSayat/Controller/ViewController.swift
@@ -0,0 +1,106 @@
+//
+//  ViewController.swift
+//  PaintAppSayat
+//
+//  Created by Аршат Талап on 04.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    let canvas = Canvas()
+    
+    let undoButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Undo", for: .normal)
+        button.titleLabel?.font = .boldSystemFont(ofSize: 14)
+        button.addTarget(self, action: #selector(undoPressed), for: .touchUpInside)
+        return button
+    }()
+    
+    @objc private func undoPressed() {
+        canvas.undo()
+    }
+    
+    let clearButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Clear", for: .normal)
+        button.titleLabel?.font = .boldSystemFont(ofSize: 14)
+        button.addTarget(self, action: #selector(clearPressed), for: .touchUpInside)
+        return button
+    }()
+    
+    @objc private func clearPressed() {
+        canvas.clear()
+    }
+    
+    let yellowButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.backgroundColor = .yellow
+        button.layer.borderWidth = 1
+        button.addTarget(self, action: #selector(handleColorChange), for: .touchUpInside)
+        return button
+    }()
+    
+    let redButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.backgroundColor = .red
+        button.layer.borderWidth = 1
+        button.addTarget(self, action: #selector(handleColorChange), for: .touchUpInside)
+        return button
+    }()
+    
+    let blueButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.backgroundColor = .blue
+        button.layer.borderWidth = 1
+        button.addTarget(self, action: #selector(handleColorChange), for: .touchUpInside)
+        return button
+    }()
+    
+    @objc private func handleColorChange(button: UIButton) {
+        let strokeColor = button.backgroundColor ?? .black
+        canvas.setStrokeColor(color: strokeColor)
+    }
+    
+    let slider: UISlider = {
+        let slider = UISlider()
+        slider.minimumValue = 1
+        slider.maximumValue = 10
+        slider.addTarget(self, action: #selector(handleSliderChange), for: .valueChanged)
+        return slider
+    }()
+    
+    @objc private func handleSliderChange() {
+        canvas.setStrokeWidth(width: slider.value)
+    }
+    
+    override func loadView() {
+        self.view = canvas
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        canvas.backgroundColor = .white
+        
+        setupLayout()
+    }
+    
+    fileprivate func setupLayout() {
+        let colorsStackView = UIStackView(arrangedSubviews: [yellowButton, redButton, blueButton])
+        colorsStackView.distribution = .fillEqually
+        
+        let stackView = UIStackView(arrangedSubviews: [undoButton, clearButton, colorsStackView, slider])
+        stackView.spacing = 8
+        stackView.distribution = .fillEqually
+        
+        view.addSubview(stackView)
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        stackView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor).isActive = true
+        stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -8).isActive = true
+    }
+    
+}
+
---
+++ PaintAppSayat/PaintAppSayat/Models/Line.swift
@@ -0,0 +1,14 @@
+//
+//  Line.swift
+//  PaintAppSayat
+//
+//  Created by Аршат Талап on 06.02.2022.
+//
+
+import UIKit
+
+struct Line{
+    let strokeWidth: Float
+    let color: UIColor
+    var points: [CGPoint]
+}
